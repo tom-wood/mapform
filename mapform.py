302c62cd5a2a9ff246b383d7e23e61b2dffd7d07
Tom Wood <thomas.wood@stfc.ac.uk>
2017-01-04 18:12:28 +0000

    Added animate_anion_costs() method

--- mapform.py
+++ mapform.py
@@ -1,9 +1,11 @@
-#Version 0.2.1 beta 
-#Changes made 03/01/17: fixed bug in plot_bonds()
+#Version 0.2.2 alpha
+#Changes made 04/01/17: refashioned plot_anion_costs() to use quiver3d;
+#added animate_anion_costs() function
 
 import numpy as np
 from mayavi import mlab
 import itertools as it
+import time
 
 class FccStructure:
     def __init__(self, anions, cations, key={2:[(0.3, 0.3, 0.3), 0.5],
@@ -122,8 +124,8 @@ class FccStructure:
                                        * self.scale)
                         mlab.plot3d(x, y, z, color=bond_colour, 
                                     tube_radius=bond_radius)
-    def plot_anion_costs(self, colour_min=(0, 0, 1), colour_max=(1, 0, 0),
-                         colour_zero=(0, 0, 0)):
+    def plot_anion_costs_old(self, colour_min=(0, 0, 1), 
+                             colour_max=(1, 0, 0), colour_zero=(0, 0, 0)):
         """Plots anion costs on anion positions by size and colours"""
         xyz = self.repeat_array(self.anions).nonzero()
         costs = self.repeat_array(self.costs)[xyz]
@@ -142,6 +144,84 @@ class FccStructure:
                 mlab.points3d(xyz[0][i], xyz[1][i], xyz[2][i], 
                               resolution=32, color=colour_zero,
                               scale_factor=costs_s[i])
+        return
+    def _get_anion_cost_cols(self, repeated_costs, colour_min=(0, 0, 1), 
+                             colour_max=(1, 0, 0), colour_zero=(0, 0, 0)):
+        """Return look-up table of anion cost colours"""
+        colours = []
+        for c in repeated_costs: 
+            if c < 0:
+                colours.append(colour_min)
+            elif c > 0:
+                colours.append(colour_max)
+            else:
+                colours.append(colour_zero)
+        colours = (np.column_stack((np.array(colours), 
+                                   np.ones(len(colours)))) *\
+                   255).astype(np.uint8)
+        return colours
+
+    def plot_anion_costs(self, colour_min=(0, 0, 1), colour_max=(1, 0, 0),
+                         colour_zero=(0, 0, 0)):
+        """Plots anion costs on anion positions by size and colours"""
+        xyz = self.repeat_array(self.anions).nonzero()
+        costs = self.repeat_array(self.costs)[xyz]
+        #costs_s = (1.1 * np.abs(costs)) / np.abs(costs).max() + 0.1
+        costs_s = (costs + 0.5) * 0.1
+        xyz = [arr * self.scale for arr in xyz]
+        colours = self._get_anion_cost_cols(costs, colour_min, colour_max, 
+                                            colour_zero)
+        scalars = np.arange(len(colours))
+        pts = mlab.quiver3d(xyz[0], xyz[1], xyz[2], costs_s, costs_s,
+                            costs_s, scalars=scalars, mode='sphere',
+                            resolution=32, scale_factor=1)
+        pts.glyph.color_mode = 'color_by_scalar'
+        pts.module_manager.scalar_lut_manager.lut.table = colours
+        mlab.draw()
+        return pts
+    def animate_anion_costs(self, anion_costs, colour_min=(0, 0, 1), 
+                            colour_max=(1, 0, 0), colour_zero=(0, 0, 0)):
+        """Animates anion cost functions
+        
+        Args:
+            anion_costs: array of series of anion costs of shape 
+            (X, N, N, N), where X is number of anion cost matrices.
+            colour_min (tup): normalized rgb value of minimum cost colour
+            (defaults to blue).
+            colour_max (tup): normalized rgb value of maximum cost colour
+            (defaults to red).
+            colour_zero (tup): normalized rgb value of zero cost colour
+            (defaults to black).
+        """
+        f = mlab.gcf()
+        xyz = self.repeat_array(self.anions).nonzero()
+        cost_arrs = [self.repeat_array(c)[xyz] for c in anion_costs]
+        cost_arrs_s = [(c + 0.5) * 0.5 for c in cost_arrs]
+        xyz = [arr * self.scale for arr in xyz]
+        col_lists = [self._get_anion_cost_cols(c, colour_min, colour_max,
+                                               colour_zero) for c in
+                     cost_arrs]
+        scalars = np.arange(len(col_lists[0]))
+        #plot first in list
+        f.scene.disable_render=True
+        pts = mlab.quiver3d(xyz[0], xyz[1], xyz[2], cost_arrs_s[0], 
+                            cost_arrs_s[0], cost_arrs_s[0], scalars=scalars,
+                            mode='sphere', resolution=32, scale_factor=1)
+        pts.glyph.color_mode = 'color_by_scalar'
+        pts.module_manager.scalar_lut_manager.lut.table = col_lists[0]
+        mlab.draw()
+        f.scene.disable_render = False
+        #now set the costs_s and look-up table values for each anion_costs
+        for i in range(1, len(cost_arrs)):
+            time.sleep(1)
+            f.scene.disable_render=True
+            pts.mlab_source.set(u=cost_arrs_s[i], v=cost_arrs_s[i],
+                                w=cost_arrs_s[i])
+            pts.module_manager.scalar_lut_manager.lut.table = col_lists[i]
+            mlab.draw()
+            f.scene.disable_render = False
+        return
+
     def plot_anion_costs_cmap(self, cm='seismic'):
         """Plots anion costs on anion positions by size and colourmap"""
         xyz = self.repeat_array(self.anions).nonzero()
